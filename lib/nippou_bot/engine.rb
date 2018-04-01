52cf68a778ec38cebb52ef31c81d75dc2e1af91c
meriy100 <ttattataa@gmail.com>
2018-04-01 23:01:59 +0900

    例外処理 + info

--- lib/nippou_bot/engine.rb
+++ lib/nippou_bot/engine.rb
@@ -5,29 +5,49 @@ require 'pry'
 
 module NippouBot
   class Engine < SlackRubyBot::Bot
-    command 'report hi' do |client, data, match|
+    command 'report' do |client, data, _|
       client.say(text: '本日の作業内容を教えてください', channel: data.channel)
     end
 
-    command 'ping' do |client, data, match|
+    command 'info' do |client, data, _|
+      begin
+        client.say(text: message, channel: data.channel)
+      rescue => e
+        client.say(text: e, channel: data.channel)
+      end
+    end
+
+    command 'ping' do |client, data, _|
       client.say(text: 'pong', channel: data.channel)
     end
 
-    scan(/(.*)/) do |client, data, match|
-      next_message = NippouBot::SlackAPI.new.next_message(data.channel, data.ts)
-      case next_message
-      when :end
-        reports = NippouBot::SlackAPI.new.get_reports(data.channel, data.ts)
-        github_events = NippouBot::Github.events
-        reports.merge!('github_events' => github_events )
-        md = NippouBot::Generator.generate(reports)
-        url = NippouBot::Esa.ship_it!(md)
-        client.say(text: url, channel: data.channel)
-      when :nothing
-        client.say(text: "Nothing", channel: data.channel)
-      else
-        client.say(text: next_message, channel: data.channel)
+    scan(/(.*)/) do |client, data, _|
+      begin
+        next_message = NippouBot::SlackAPI.new.next_message(data.channel, data.ts)
+        case next_message
+        when :end
+          reports = NippouBot::SlackAPI.new.get_reports(data.channel, data.ts)
+          github_events = NippouBot::Github.events
+          reports['github_events'] = github_events
+          md = NippouBot::Generator.generate(reports)
+          url = NippouBot::Esa.ship_it!(md)
+          client.say(text: url, channel: data.channel)
+        when :nothing
+          client.say(text: 'Nothing', channel: data.channel)
+        else
+          client.say(text: next_message, channel: data.channel)
+        end
+      rescue => e
+        client.say(text: e, channel: data.channel)
       end
+    end
+
+    private
+
+    def info_message
+<<EOS
+users: #{NippouBot::SlackAPI.new.users}
+EOS
     end
   end
 end
